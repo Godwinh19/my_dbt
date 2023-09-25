76c07c1751a924fdadf373b73a6eb83d8822e92f
Godwin H <houdjigodwin@gmail.com>
2023-09-25 20:54:49 +0000

    update model

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -10,10 +10,9 @@
 {{ config(materialized='table') }}
 
 with source_data as (
-
-    select 1 as id
-    union all
-    select null as id
+    select new_recovered as recovered,
+    _airbyte_ab_id as id,
+    FROM {{source('covid')}}
 
 )
 
