6e38fa6be060180634564372155ee2104b42002a
Godwin H <houdjigodwin@gmail.com>
2023-09-26 20:50:36 +0000

    update model

--- models/prod/covid_dbt.sql
+++ models/prod/covid_dbt.sql
@@ -1,7 +1,25 @@
 {{ config(
   materialized='table',
   indexes = [{'columns':['_airbyte_emitted_at'],'type':'btree'}],
+  schema = "public"
    unique_key = 'id',
+   post_hook = ["
+                    {%
+                        set scd_table_relation = adapter.get_relation(
+                            database=this.database,
+                            schema=this.schema,
+                            identifier='covid_scd'
+                        )
+                    %}
+                    {%
+                        if scd_table_relation is not none
+                    %}
+                    {%
+                            do adapter.drop_relation(scd_table_relation)
+                    %}
+                    {% endif %}
+                        "],
+    tags = [ "top-level" ]
 ) }}
 
 select
