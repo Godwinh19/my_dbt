21476a23ae3768832418b3eee9d4c8882149b750
Godwin H <houdjigodwin@gmail.com>
2023-09-26 20:31:58 +0000

    update model

--- models/intermediate/merged.sql
+++ models/intermediate/merged.sql
@@ -23,9 +23,12 @@
 
 
 select
+    {{ adapter.quote('date') }},
     new_recovered,
     _airbyte_ab_id,
     _airbyte_emitted_at,
+    {{ current_timestamp() }} as _airbyte_normalized_at,
+    _airbyte_covid_hashid
 from {{ source('staging', 'covid_normalized') }}
 
 where 1 = 1
