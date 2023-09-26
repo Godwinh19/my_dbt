4931de8122829a364046e2288996dc98185c56a2
Godwin H <houdjigodwin@gmail.com>
2023-09-26 20:25:43 +0000

    update model

--- models/intermediate/merged.sql
+++ models/intermediate/merged.sql
@@ -21,23 +21,10 @@
     tags = [ "top-level" ]
 ) }}
 
-with source_data as (
 select
-    {{ adapter.quote('date') }},
-    new_recovered,
-    new_tested,
     total_deceased,
     new_deceased,
-    new_confirmed,
-    total_confirmed,
-    total_tested,
-    total_recovered,
-    {{ adapter.quote('key') }},
+
     _airbyte_ab_id,
-    _airbyte_emitted_at,
-    {{ current_timestamp() }} as _airbyte_normalized_at,
-    _airbyte_covid_hashid
 from {{ source('staging', 'covid_normalized') }}
-)
-select * from source_data
 where 1 = 1
