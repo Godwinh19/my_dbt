d3c8fc649a88a31689fec3422e3ecc70d12654fa
Godwinh19 <houdjigodwin@gmail.com>
2023-09-27 18:58:27 +0000

    update model

--- models/intermediate/merged.sql
+++ models/intermediate/merged.sql
@@ -1,24 +1,5 @@
 {{ config(
-    indexes = [{'columns':['_airbyte_emitted_at'],'type':'btree'}],
-    unique_key = '_airbyte_ab_id',
-    schema = "staging",
-    post_hook = ["
-                    {%
-                        set scd_table_relation = adapter.get_relation(
-                            database=this.database,
-                            schema=this.schema,
-                            identifier='covid_scd'
-                        )
-                    %}
-                    {%
-                        if scd_table_relation is not none
-                    %}
-                    {%
-                            do adapter.drop_relation(scd_table_relation)
-                    %}
-                    {% endif %}
-                        "],
-    tags = [ "top-level" ]
+    indexes = [{'columns':['_airbyte_ab_id'],'type':'hash'}],
 ) }}
 
 
